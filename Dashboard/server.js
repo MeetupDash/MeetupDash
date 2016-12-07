77c1873aaab587a85424f262dd3756f1c11c1390
Kartikeya Pharasi <kpharas@ncsu.edu>
2016-12-07 09:51:21 -0500

    Changing frontend main page

--- Dashboard/server.js
+++ Dashboard/server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient
+var bodyParser = require('body-parser')
 var redis = require('redis')
 var client = redis.createClient(6379, '152.46.19.205', {})
 var db
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.use(bodyParser.json());
 // MongoClient.connect('mongodb://localhost:27017/envoprov', (err, database) => {
 //   if (err) return console.log(err)
 //   db = database
@@ -22,6 +28,10 @@ MongoClient.connect('mongodb://152.46.19.205:27017/meetup', (err, database) => {
   })
 })
 
+app.get('/', (req, res) => {
+   res.render('main.ejs')
+})
+
 app.get('/:msg', (req, res) => {
   db.collection('cityEvent').find({city:req.params.msg}).toArray(function(err, results) {
   	if (err) return console.log(err)
@@ -30,10 +40,10 @@ app.get('/:msg', (req, res) => {
   })
 })
 
-app.get('/redis/findcities/:msg', (req, res) => {
+app.post('/findcities', (req, res) => {
     client.select(7);
-    client.llen(req.params.msg, function(err, len) {
-        client.lrange(req.params.msg, 0, 5, function(err, val) {
+    client.llen(req.body.events, function(err, len) {
+        client.lrange(req.body.events, 0, 5, function(err, val) {
             //res.write('Top Cities hosting this event: ' + val);
             res.render('index.ejs', {cityEvent: val})
 	    res.end();
@@ -41,10 +51,10 @@ app.get('/redis/findcities/:msg', (req, res) => {
     });
 })
 
-app.get('/redis/findevents/:msg', (req, res) => {
+app.post('/findevents', (req, res) => {
     client.select(10);
-    client.llen(req.params.msg, function(err, len) {
-        client.lrange(req.params.msg, 0, 5, function(err, val) {
+    client.llen(req.body.city, function(err, len) {
+        client.lrange(req.body.city, 0, 5, function(err, val) {
             //res.write('Top Cities hosting this event: ' + val);
             res.render('indexevents.ejs', {cityEvent_original: val})
             res.end();
